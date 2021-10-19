12636d7e15b9614f9e4193efb6f0050f8d80e8d3
Luziano Silva <luzianosilva.dv@gmail.com>
2021-10-19 13:28:49 -0300

    Comit do exercicio 01

---
+++ exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Escreva a primeira nota!");            
+    decimal nota01 = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Escreva a segunda nota!");
+    decimal nota02 = Convert.ToDecimal(Console.ReadLine());
+    decimal media = (nota01+nota02) / 2;
+    if(media == 10){
+        Console.WriteLine("Aprovado com distinção");
+    }
+    else if(media >= 7){
+        Console.WriteLine("Aprovado");
+    }
+    else{
+Console.WriteLine("Reprovado");        
+    }
+        }
+    }
+}
